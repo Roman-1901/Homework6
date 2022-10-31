0aebd135ad1edb58d5838fb60f103846d5be48ce
roman <roman-1901@mail.ru>
2022-10-31 22:35:35 +0300

    Homework6

--- src/Task3_2.java
+++ src/Task3_2.java
@@ -2,15 +2,8 @@ public class Task3_2 {
     public static void main(String[] args) {
         int money = 29000;
         double sum = 0;
-        double monthlyProfit;
         for (int i = 1; i <= 12; i++) {
-            if (i == 1) {
-                monthlyProfit = money * 0.01;
-                sum = monthlyProfit + money;
-            } else {
-                monthlyProfit = sum * 0.01;
-                sum += monthlyProfit + money;
-            }
+            sum += money + (sum + money) * 0.01;
             System.out.println("Месяц " + i + " , сумма накоплений равна " + String.format("%.2f", sum) + " рублей");
         }
     }
